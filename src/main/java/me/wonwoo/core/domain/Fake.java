cdada0fca1934986b392ecbfebb2b92cf5cd6057
wonwoo <aoruqjfu@gmail.com>
2017-03-11 21:31:58 +0900

    어노테이션 삭제

--- src/main/java/me/wonwoo/core/domain/Fake.java
+++ src/main/java/me/wonwoo/core/domain/Fake.java
@@ -1,7 +1,6 @@
 package me.wonwoo.core.domain;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -9,7 +8,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
  * Created by wonwoo on 2017. 3. 11..
  */
 @Data
-@RequiredArgsConstructor
 @Document
 public class Fake {
   @Id
