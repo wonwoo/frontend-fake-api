a581d555c0e6a7afa3f990763d2ef51bb60f4760
wonwoo <aoruqjfu@gmail.com>
2017-03-12 20:53:34 +0900

    화면 수정

--- src/main/java/me/wonwoo/web/DuplicateException.java
+++ src/main/java/me/wonwoo/web/DuplicateException.java
@@ -1,7 +1,6 @@
 package me.wonwoo.web;
 
 import lombok.RequiredArgsConstructor;
-import me.wonwoo.support.FakeParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
--- src/main/java/me/wonwoo/web/IndexController.java
+++ src/main/java/me/wonwoo/web/IndexController.java
@@ -2,15 +2,21 @@ package me.wonwoo.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.util.concurrent.ForkJoinPool;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import me.wonwoo.core.domain.Fake;
 import me.wonwoo.core.repository.FakeRepository;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -26,13 +32,22 @@ public class IndexController {
   private final ObjectMapper objectMapper;
 
   @GetMapping("/")
-  public String home(Model model) {
+  public String home(Model model, FakeForm fakeForm) {
     model.addAttribute("fakes", fakeRepository.findAll());
+    model.addAttribute("fakeForm", fakeForm);
     return "index";
   }
 
+  @GetMapping("/{id}")
+  public ResponseEntity<?> home(@PathVariable String id) {
+    return ResponseEntity.ok(fakeRepository.findOne(id)) ;
+  }
+
   @PostMapping("/fakes")
-  public String save(@RequestBody @Valid FakeForm fakeForm) {
+  public String save(@ModelAttribute @Valid FakeForm fakeForm, BindingResult bindingResult) {
+    if(bindingResult.hasErrors()){
+      return "index";
+    }
     if (fakeRepository.findByUriAndMethod(fakeForm.getUri(), fakeForm.getMethod())
       .isPresent()) {
       throw new DuplicateException(fakeForm);
@@ -43,7 +58,7 @@ public class IndexController {
       HttpStatus.valueOf(fakeForm.getStatusCode()),
       isValidJson(fakeForm.getData())));
 
-    return "index";
+    return "redirect:/#/";
   }
 
   private String isValidJson(final String json) {
