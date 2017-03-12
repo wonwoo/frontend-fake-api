65c282185b08ff55900ef7f0e7c5ae96e05f2e5b
wonwoo <aoruqjfu@gmail.com>
2017-03-12 12:42:58 +0900

    json valid 추가 및 화면 수정

--- src/main/java/me/wonwoo/web/IndexController.java
+++ src/main/java/me/wonwoo/web/IndexController.java
@@ -1,6 +1,7 @@
 package me.wonwoo.web;
 
-import java.util.Collection;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import me.wonwoo.core.domain.Fake;
 import me.wonwoo.core.repository.FakeRepository;
@@ -9,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by wonwoo on 2017. 3. 11..
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
   private final FakeRepository fakeRepository;
+  private final ObjectMapper objectMapper;
 
   @GetMapping("/")
   public String home(Model model) {
@@ -30,6 +31,15 @@ public class IndexController {
   //FIXME test
   @PostMapping("/test")
   public void save(@RequestBody FakeForm fakeForm) {
-    fakeRepository.save(new Fake(fakeForm.getUri(), fakeForm.getMethod(), fakeForm.getData()));
+    fakeRepository.save(new Fake(fakeForm.getUri(), fakeForm.getMethod(), isValidJson(fakeForm.getData())));
+  }
+
+  private String isValidJson(final String json) {
+    try {
+      objectMapper.readTree(json);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    return json;
   }
 }
