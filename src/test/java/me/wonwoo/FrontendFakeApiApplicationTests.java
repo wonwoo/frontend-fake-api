fe825ddfa7e756c62a989988de9c257569d318fd
wonwoo <aoruqjfu@gmail.com>
2017-03-14 23:16:47 +0900

    test 케이스 추가 추후에 테스트 케이스 작성

--- src/test/java/me/wonwoo/FrontendFakeApiApplicationTests.java
+++ src/test/java/me/wonwoo/FrontendFakeApiApplicationTests.java
@@ -1,11 +1,25 @@
 package me.wonwoo;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class FrontendFakeApiApplicationTests {
 
+  @Autowired
+  private ApplicationContext applicationContext;
+
+  @Test
+  public void applicationTest() {
+    assertThat(this.applicationContext.getBeansOfType(SpringDataDialect.class)).isNotEmpty();
+  }
 }
